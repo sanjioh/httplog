206e1b1e34e839fa1aa11f1b44b46e525d693b42
Fabio Sangiovanni <sjh+github@sanjioh.org>
2018-03-25 14:27:09 +0200

    refactor LogRecord

--- src/httplog/parsers.py
+++ src/httplog/parsers.py
@@ -1,23 +1,15 @@
 import re
 
+_LOG_RE = r'^(\S+) \S+ (\S+) \[[^\]]+\] "[A-Z]+ ([^ "]+)? HTTP/[0-9.]+" [0-9]{3} ([0-9]+|-)'  # noqa
+
 
 class LogRecord:
-    _regexp = re.compile(  # TODO constant?
-        r'^(\S+) (\S+) (\S+) \[([^\]]+)\] '
-        r'"([A-Z]+) ([^ "]+)? HTTP/[0-9.]+" '
-        r'([0-9]{3}) ([0-9]+|-)',
-    )
+    _regexp = re.compile(_LOG_RE)
 
     def __init__(self, line):
         match = self._regexp.match(line)
-        (host, ident, authuser, date, method,
-         resource, status, size) = match.groups()
+        host, authuser, resource, size = match.groups()
         self.host = host
-        self.ident = ident
         self.authuser = authuser
-        self.date = date
-        self.method = method
-        self.resource = resource
-        self.status = status
         self.size = int(size)
         self.section = f"/{resource.split('/', 2)[1]}"
