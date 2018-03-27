7eb90171c177aacd82488906d9aed2825865eaba
Fabio Sangiovanni <sjh+github@sanjioh.org>
2018-03-27 22:16:57 +0200

    docstrings

--- tests/test_alertobserver.py
+++ tests/test_alertobserver.py
@@ -1,3 +1,4 @@
+"""Tests for httplog.observers.AlertObserver."""
 import io
 import time
 
@@ -6,8 +7,8 @@ import pytest
 from httplog.observers import AlertObserver
 
 
-class DummyLogEntry:
-    pass
+class DummyLogRecord:
+    """LogRecord placeholder."""
 
 
 @pytest.fixture
@@ -30,6 +31,10 @@ def formatter():
 @pytest.fixture
 def event():
     class FakeEvent:
+        """
+        Fake event class which enables fine control over the child thread
+        execution.
+        """
         def __init__(self):
             self._flag = False
             self._waiting = False
@@ -55,64 +60,87 @@ def event():
 
 
 def test_no_alerts(fd, formatter, event):
+    """
+    AlertObserver should print no alerts at all if it is not notified
+    of any log entry.
+    """
     ao = AlertObserver(3, fd=fd, formatter=formatter, event=event)
     ao.start()
-    event.run_until_wait()
+    event.run_until_wait()  # no print
     event.stop()
     assert fd.getvalue() == ''
 
 
 def test_alert_high(fd, formatter, event):
+    """
+    AlertObserver should print an 'alarm triggered' alert if
+    the number of log entries goes beyond the threshold.
+    """
     ao = AlertObserver(3, fd=fd, formatter=formatter, event=event)
     ao.start()
     event.run_until_wait()
     for _ in range(360):
-        ao.update(DummyLogEntry())
-    event.run_until_wait()
+        ao.update(DummyLogRecord())
+    event.run_until_wait()  # 'alarm triggered'
     event.stop()
     assert fd.getvalue() == 'alarm triggered\n'
 
 
 def test_alert_high_doesnt_fire_twice(fd, formatter, event):
+    """
+    AlertObserver should print an 'alarm triggered' alert only once if
+    the number of log entries goes beyond the threshold multiple times
+    in a row.
+    """
     ao = AlertObserver(3, fd=fd, formatter=formatter, event=event)
     ao.start()
     event.run_until_wait()
     for _ in range(360):
-        ao.update(DummyLogEntry())
-    event.run_until_wait()
+        ao.update(DummyLogRecord())
+    event.run_until_wait()  # 'alarm triggered'
     for _ in range(360):
-        ao.update(DummyLogEntry())
-    event.run_until_wait()
+        ao.update(DummyLogRecord())
+    event.run_until_wait()  # no print
     event.stop()
     assert fd.getvalue() == 'alarm triggered\n'
 
 
 def test_alert_low(fd, formatter, event):
+    """
+    AlertObserver should print an 'alarm triggered' alert followed by
+    an 'alarm recovered' alert if the number of log entries goes beyond the
+    threshold and then comes back to normal.
+    """
     ao = AlertObserver(3, fd=fd, formatter=formatter, event=event)
     ao.start()
     event.run_until_wait()
     for _ in range(360):
-        ao.update(DummyLogEntry())
-    event.run_until_wait()
+        ao.update(DummyLogRecord())
+    event.run_until_wait()  # 'alarm triggered'
     for _ in range(359):
-        ao.update(DummyLogEntry())
-    event.run_until_wait()
+        ao.update(DummyLogRecord())
+    event.run_until_wait()  # 'alarm recovered'
     event.stop()
     assert fd.getvalue() == 'alarm triggered\nalarm recovered\n'
 
 
 def test_alert_low_doesnt_fire_twice(fd, formatter, event):
+    """
+    AlertObserver should print an 'alarm recovered' alert only once if
+    the number of log entries goes beyond the threshold and then stays below it
+    multiple times in a row.
+    """
     ao = AlertObserver(3, fd=fd, formatter=formatter, event=event)
     ao.start()
     event.run_until_wait()
     for _ in range(360):
-        ao.update(DummyLogEntry())
-    event.run_until_wait()
+        ao.update(DummyLogRecord())
+    event.run_until_wait()  # 'alarm triggered'
     for _ in range(359):
-        ao.update(DummyLogEntry())
-    event.run_until_wait()
+        ao.update(DummyLogRecord())
+    event.run_until_wait()  # 'alarm recovered'
     for _ in range(359):
-        ao.update(DummyLogEntry())
-    event.run_until_wait()
+        ao.update(DummyLogRecord())
+    event.run_until_wait()  # no print
     event.stop()
     assert fd.getvalue() == 'alarm triggered\nalarm recovered\n'
